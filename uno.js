882e25ebdffddd43d8e47f9e8f734b6c8f26f69c
antonellamichetti <68030606+antonellamichetti@users.noreply.github.com>
2020-08-10 19:29:14 -0300

    Add files via upload

--- uno.js
+++ uno.js
@@ -9,5 +9,55 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+let nombre;
+let peso;
+let sexo;
+let edad;
+let i;
+let contadorHombres = 0;
+let contadorMujeres = 0;
+let acuEdad = 0;
+let banderaHombrePeso = 0;
+let mayorPesoHombre;
+let nombreHombrePesado;
+let mensajeHombrePesado = "No se ingresaron hombres";
+
+for (i = 0; i < 5; i++) {
+
+    nombre = prompt("Ingrese nombre: ");
+	peso = parseFloat(prompt("Ingrese peso: "));
+	while (peso < 0) {
+		peso = parseFloat(prompt("Error. Ingrese peso: "));
+	}
+    sexo = prompt("Ingrese sexo -f/m-");
+    while (sexo != "f" && sexo != "m") {
+        sexo = prompt("Error. Ingrese sexo -f/m-");
+    }
+    edad = parseInt(prompt("Ingrese edad: "));
+    while (edad < 1 || edad > 120 || isNaN(edad)) {
+        edad = parseInt(prompt("Error. Ingrese edad: "));
+    }
+    if (sexo == "f") {
+        contadorMujeres++;
+    
+    }
+    else {
+		contadorHombres++;
+		if (banderaHombrePeso == 0 || peso > mayorPesoHombre) {
+		   mayorPesoHombre = peso;
+		   nombreHombrePesado = nombre;
+            banderaHombrePeso = 1;
+        }
+    }
+    acuEdad = acuEdad + edad;
+}
+
+if (banderaHombrePeso == 1) {
+    mensajeHombrePesado = "Hombre con mayor peso: "+nombreHombrePesado+", peso: "+mayorPesoHombre;
+}
+
+document.write("A- Cantidad de mujeres: " + contadorMujeres + "<br>");
+document.write("B- Edad promedio: " + (acuEdad / 5)+"<br>");
+document.write("C - "+mensajeHombrePesado);
+
 }
